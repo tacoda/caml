346be0afe7131caab72aece8f92aeac4d79310ab
Ian Johnson <ijohnson@covermymeds.com>
2020-12-10 13:20:03 -0500

    Add serde yaml crate and basic example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
+use serde::{Serialize, Deserialize};
 
 /// A basic example
 #[derive(StructOpt, Debug)]
@@ -40,7 +41,28 @@ struct Opt {
     files: Vec<PathBuf>,
 }
 
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+fn serialize_and_deserialize_point() -> Result<(), serde_yaml::Error> {
+    let point = Point { x: 1.0, y: 2.0 };
+
+    let s = serde_yaml::to_string(&point)?;
+    assert_eq!(s, "---\nx: 1.0\ny: 2.0");
+
+    let deserialized_point: Point = serde_yaml::from_str(&s)?;
+    assert_eq!(point, deserialized_point);
+    Ok(())
+}
+
 fn main() {
     let opt = Opt::from_args();
     println!("{:#?}", opt);
+    match serialize_and_deserialize_point() {
+        Err(_e) => println!("Failure"),
+        Ok(_r) => println!("Success"),
+    }
 }
