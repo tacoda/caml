9769b2e40ab572c3f18c6eb2d0dcc8a3d75b96c0
Ian Johnson <ijohnson@covermymeds.com>
2020-12-10 17:06:20 -0500

    Add configuration loading

--- src/caml/config.rs
+++ src/caml/config.rs
@@ -1,29 +1,45 @@
+use std::path::PathBuf;
 use serde::{Serialize, Deserialize};
 
-pub fn load() {
-    match serialize_and_deserialize_point() {
-        Err(_e) => println!("Point serialize failed"),
-        Ok(_r) => println!("Point serialize succeeded"),
+pub fn load() -> Result<Point, serde_yaml::Error> {
+    let s = "---\nx: 1.0\ny: 2.0";
+    let point: Point = serde_yaml::from_str(&s)?;
+    Ok(point)
+}
+
+pub fn init(directory: &Option<PathBuf>) {
+    match directory {
+        Some(dir) => { println!("Init in the {:?} directory.", dir); },
+        None => { println!("Init in the current directory!"); },
     }
 }
 
-fn find_config() {
-    //
-}
+// fn find_config() {
+//     //
+// }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct Point {
+pub struct Point {
     x: f64,
     y: f64,
 }
 
-fn serialize_and_deserialize_point() -> Result<(), serde_yaml::Error> {
-    let point = Point { x: 1.0, y: 2.0 };
+// fn serialize_and_deserialize_point() -> Result<(), serde_yaml::Error> {
+//     let point = Point { x: 1.0, y: 2.0 };
 
-    let s = serde_yaml::to_string(&point)?;
-    assert_eq!(s, "---\nx: 1.0\ny: 2.0");
+//     let s = serde_yaml::to_string(&point)?;
+//     assert_eq!(s, "---\nx: 1.0\ny: 2.0");
 
-    let deserialized_point: Point = serde_yaml::from_str(&s)?;
-    assert_eq!(point, deserialized_point);
-    Ok(())
-}
+//     let deserialized_point: Point = serde_yaml::from_str(&s)?;
+//     assert_eq!(point, deserialized_point);
+//     Ok(())
+// }
+
+// use std::fs::File;
+// use std::io::prelude::*;
+
+// fn write_file() -> std::io::Result<()> {
+//     let mut file = File::create("foo.txt")?;
+//     file.write_all(b"Hello, world!")?;
+//     Ok(())
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
-// use std::path::PathBuf;
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 mod caml;
 use crate::caml as caml_lib;
+
+macro_rules! subcommand
+{
+    ( $( $x:expr ),* ) => {
+        {
+            let mut result = 0.0;
+            $(
+                result += $x as f64;
+            )*
+            result as i64
+        }
+    };
+}
+
+macro_rules! dispatcher
+{
+    ( $( $x:expr ),* ) => {
+        {
+            let mut result = 0.0;
+            $(
+                result += $x as f64;
+            )*
+            result as i64
+        }
+    };
+}
 
 #[derive(StructOpt)]
 #[structopt(name = "caml", about = "A dynamic CLI tool defined with declarative YAML")]
@@ -11,13 +37,29 @@ enum Caml {
     Hello,
     #[structopt(name = "config", about = "Configuration")]
     Config,
+    #[structopt(name = "init", about = "Initialize a directory for caml")]
+    Init {
+        #[structopt(parse(from_os_str))]
+        /// Directory to initialize (Default: current directory)
+        directory: Option<PathBuf>,
+    }
+    // Reserved: init, help, show?, audit?
 }
 
 fn main() {
     let caml = Caml::from_args();
 
-    match caml {
-        Caml::Hello => { caml_lib::command::hello_world() },
-        Caml::Config => { caml_lib::config::load() },
+    if let Caml::Init { ref directory } = caml {
+        caml_lib::config::init(directory);
+    } else {
+        let config = caml_lib::config::load();
+
+        match caml {
+            Caml::Hello => { caml_lib::command::hello_world() },
+            Caml::Config => { println!("{:?}", config); },
+            // Caml::Init { directory } => { caml_lib::config::init(directory) },
+            _ => {},
+        }
     }
+
 }
